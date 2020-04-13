d3fdb42d084fa50c73ff12169a0f10115d28747f
Robert Walsh <rwalsh@excalibur-solutions.com>
2020-04-13 15:32:24 -0400

    Add Storybook stories for components

---
+++ stories/Checklist.stories.js
@@ -0,0 +1,20 @@
+import { action } from '@storybook/addon-actions';
+import Checklist from '../src/Checklist.svelte';
+import StyleWrapper from './StyleWrapper.svelte';
+import '../public/global.css';
+
+export default { title: 'Checklist' };
+
+export const basic = () => ({
+    Component: StyleWrapper,
+    props: {
+        component: Checklist,
+        style: `
+            background-color: cornflowerblue;
+            color: white;
+            height: 100vh;
+            padding: 1rem;
+        `
+    },
+    on: { logout: action('logout dispatched') }
+});
